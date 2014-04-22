adf95ca272fe70f7b86aa6417c4b0a0c6d478061
Yanis Wang <yanis.wang@gmail.com>
2014-04-22 15:30:59 +0800

    fix for https url

--- jsunit.js
+++ jsunit.js
@@ -5,21 +5,22 @@ module.exports = function(pagerun){
     var bOpenUrl = false;
     pagerun.on('proxyStart', function(msg){
         var proxy = msg.proxy;
-        var jsunitPath = 'http://pagerun/jsunit/';
-        // Mocha
-        var adapterMochaContent = fs.readFileSync(__dirname+'/jsunit-adapter-mocha.js');
-        var scriptMocha = '<script type="text/javascript" src="'+jsunitPath+'adapter_mocha.js" charset="utf-8"></script>';
-        // Jasmine
-        var adapterJasmineContent = fs.readFileSync(__dirname+'/jsunit-adapter-jasmine.js');
-        var scriptJasmine = '<script type="text/javascript" src="'+jsunitPath+'adapter_jasmine.js" charset="utf-8"></script>';
-        //QUnit
-        var adapterQunitContent = fs.readFileSync(__dirname+'/jsunit-adapter-qunit.js');
-        var scriptQunit = '<script type="text/javascript" src="'+jsunitPath+'adapter_qunit.js" charset="utf-8"></script>';
-        //YUITest
-        var adapterYuitestContent = fs.readFileSync(__dirname+'/jsunit-adapter-yuitest.js');
-        var scriptYUITest = '<script type="text/javascript" src="'+jsunitPath+'adapter_yuitest.js" charset="utf-8"></script>';
         proxy.addFilter(function(httpData, next, end){
             if(bOpenUrl === true){
+                var jsunitPath = httpData.protocol+'//pagerun/jsunit/';
+                // Mocha
+                var adapterMochaContent = fs.readFileSync(__dirname+'/jsunit-adapter-mocha.js');
+                var scriptMocha = '<script type="text/javascript" src="'+jsunitPath+'adapter_mocha.js" charset="utf-8"></script>';
+                // Jasmine
+                var adapterJasmineContent = fs.readFileSync(__dirname+'/jsunit-adapter-jasmine.js');
+                var scriptJasmine = '<script type="text/javascript" src="'+jsunitPath+'adapter_jasmine.js" charset="utf-8"></script>';
+                //QUnit
+                var adapterQunitContent = fs.readFileSync(__dirname+'/jsunit-adapter-qunit.js');
+                var scriptQunit = '<script type="text/javascript" src="'+jsunitPath+'adapter_qunit.js" charset="utf-8"></script>';
+                //YUITest
+                var adapterYuitestContent = fs.readFileSync(__dirname+'/jsunit-adapter-yuitest.js');
+                var scriptYUITest = '<script type="text/javascript" src="'+jsunitPath+'adapter_yuitest.js" charset="utf-8"></script>';
+                
                 var responseContent = httpData.responseContent;
                 if(httpData.responseCode === 200 &&
                     httpData.responseType === 'html' &&
